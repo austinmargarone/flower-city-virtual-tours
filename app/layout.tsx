4f84742dadaf55022845362b2621940083e88324
fcvirtual <austin@fcvt.dev>
2023-11-01 09:41:13 -0400

    Layout & Middleware for authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import React from "react";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import ReduxProvider from "../redux/ReduxProvider";
 import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,14 +18,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <ReduxProvider>
-          <Navbar />
-
-          {children}
-        </ReduxProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <Navbar />
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
