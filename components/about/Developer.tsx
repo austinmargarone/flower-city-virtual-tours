6771e5564881d5e40c03e40d1354ade9027907f1
fcvirtual <austin@fcvt.dev>
2024-02-13 11:58:20 -0500

    About Page

--- components/about/Developer.tsx
+++ components/about/Developer.tsx
@@ -6,22 +6,19 @@ const Developer = () => {
   return (
     <section>
       <h2 className="flex justify-center">Lead Full-Stack Devloper</h2>
-      <Link href="https://github.com/austinmargarone" target="_blank"></Link>
-      <p className="flex justify-center">Austin Margarone</p>
-      <Image
-        src="/images/austin-margarone.jpg"
-        alt="Austin Margarone"
-        width={200}
-        height={200}
-        className="rounded-full"
-      />
-      <div className="flex gap-[2.25rem] pl-[2.5rem] pt-[.88rem]">
-        {/* <Link
-          href="https://www.linkedin.com/in/austin-margarone/"
-          target="_blank"
-        > */}
-        <Image src={"/Social.svg"} width={24} height={24} alt="Github Link" />
-        {/* </Link> */}
+      <div className="flex justify-center">
+        <Image
+          src="/Developer.jpeg"
+          alt="Austin Margarone"
+          width={200}
+          height={200}
+          className="rounded-full flex justify-center"
+        />
+      </div>
+      <div className="flex justify-center mt-[1.25rem]">
+        <Link href="https://austinmargarone.com/" target="_blank">
+          <button className="hero-button shadow-lg">Austin Margarone</button>
+        </Link>
       </div>
     </section>
   );
