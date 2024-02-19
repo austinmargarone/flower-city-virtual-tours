21d464ff805436f79b5baf3820b33fd02c54a317
fcvirtual <austin@fcvt.dev>
2024-02-19 15:26:31 -0500

    feat: Toast for Number

--- components/CopyNumber.tsx
+++ components/CopyNumber.tsx
@@ -4,10 +4,15 @@ import React from "react";
 const CopyNumber = () => {
   function copyText(entryText: string) {
     navigator.clipboard.writeText(entryText);
+    alert("Number copied to clipboard!");
   }
+
   return (
     <>
-      <button onClick={() => copyText("5855015690")} className="w-full">
+      <button
+        onClick={() => copyText("5855015690")}
+        className="w-full bg-[#B49167] shadow-lg p-[.25rem] rounded-[.17rem] text-dark-midnight"
+      >
         <p className="text-white">C: (585) 501-5690</p>
       </button>
     </>
