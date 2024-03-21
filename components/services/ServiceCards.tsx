0ce41a067ada11e7ef9cd1f6baf7ce1a04a1bb20
fcvirtual <austin@fcvt.dev>
2024-03-21 15:31:05 -0400

    Feat: Smaller service cards

--- components/services/ServiceCards.tsx
+++ components/services/ServiceCards.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import WebDev from "./cards/WebDev";
+import Contracts from "./cards/Contracts";
+import VirtualTours from "./cards/VirtualTours";
+import Photography from "./cards/Photography";
 
 const ServiceCards = () => {
-  return <div>ServiceCards</div>;
+  return (
+    <div className="flex flex-col gap-[1.25rem] mx-auto">
+      <div>
+        <WebDev />
+      </div>
+      <div>
+        <Contracts />
+      </div>
+      <div>
+        <VirtualTours />
+      </div>
+      <div>
+        <Photography />
+      </div>
+    </div>
+  );
 };
 
 export default ServiceCards;
