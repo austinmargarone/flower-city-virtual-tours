e6c71c6da68d448a1418f9d6f368b4151f68b5f2
fcvirtual <austin@fcvt.dev>
2024-03-22 18:34:50 -0400

    feat: Add Matterport Data

--- app/(root)/virtual-tours/page.tsx
+++ app/(root)/virtual-tours/page.tsx
@@ -4,6 +4,9 @@ import React from "react";
 const page = () => {
   return (
     <div>
+      <h2 className="flex justify-center pt-[1.25rem] md:pt-[2.5rem]">
+        Virtual Tours
+      </h2>
       <VirtualTours />
     </div>
   );
--- components/services/virtual tours/VirtualTours.tsx
+++ components/services/virtual tours/VirtualTours.tsx
@@ -8,10 +8,56 @@ const VirtualTours = () => {
       embed: "https://my.matterport.com/show/?m=Vu8vAVvcebN",
     },
     {
-      embed: "https://my.matterport.com/show/?m=Vu8vAVvcebN",
+      // Aarons Alley
+      embed: "https://my.matterport.com/show/?m=Yug3Wm4ZsKC",
     },
     {
-      embed: "https://my.matterport.com/show/?m=Vu8vAVvcebN",
+      // Guest Barn
+      embed: "https://my.matterport.com/show/?m=YN4DWeXcQ2p",
+    },
+    {
+      // HDP
+      embed: "https://my.matterport.com/show/?m=MnvsFYd5SXE",
+    },
+    {
+      // DeCarolis Buffalo
+      embed: "https://my.matterport.com/show/?m=dTursFxbP1u",
+    },
+    {
+      // Serendipity Labs
+      embed: "https://my.matterport.com/show/?m=WvpyR9oxStU",
+    },
+    {
+      // IronChiro
+      embed: "https://my.matterport.com/show/?m=KCEzCvbnQc2",
+    },
+    {
+      // MCH
+      embed: "https://my.matterport.com/show/?m=MRKsLeTQbbN",
+    },
+    {
+      // The Nest Cottage
+      embed: "https://my.matterport.com/show/?m=zN2vzBPQ6kP",
+    },
+    {
+      // 650 Wild Mallard
+      embed: "https://my.matterport.com/show/?m=1A8zFLesAh3",
+    },
+    {
+      // 609 Wild Mallard
+      embed: "https://my.matterport.com/show/?m=Xjuo4yMEVqQ",
+    },
+    {
+      // 28 Great Oaks Lane
+      embed: "https://my.matterport.com/show/?m=qzjkH94yzsP",
+    },
+    {
+      // Strawberry Castle
+      embed: "https://my.matterport.com/show/?m=f7ZjbwNGHrp",
+    },
+    {
+      // Strawberry Castle Garage
+      embed: "https://my.matterport.com/show/?m=KPP8WwjcLc4",
     },
   ];
 
