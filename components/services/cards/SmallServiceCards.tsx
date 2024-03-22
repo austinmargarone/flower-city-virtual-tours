fae0fbf5c09cfd599e62e82d03b1b9230164ff7c
fcvirtual <austin@fcvt.dev>
2024-03-22 17:42:30 -0400

    Feat: Shadow style and desktop view:

--- components/services/cards/SmallServiceCards.tsx
+++ components/services/cards/SmallServiceCards.tsx
@@ -14,7 +14,7 @@ const SmallServiceCards: React.FC<Props> = ({ ServiceData }) => {
   const { src, alt, name, description } = ServiceData;
 
   return (
-    <div className="bg-white h-full shadow-lg rounded-lg flex flex-col w-full p-[1rem]">
+    <div className="bg-white h-[18rem] shadow-lg rounded-lg flex flex-col w-full p-[1rem] hover:shadow-lg hover:shadow-[#0D101E] min-w-[18rem] max-w-[25rem]">
       <div className="flex items-center justify-center">
         <Image src={src} alt={alt} width={150} height={150} />
       </div>
