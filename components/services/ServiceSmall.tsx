c66b1d705f28ea25a1e413489b6054445bbb6b99
fcvirtual <austin@fcvt.dev>
2024-03-22 17:12:35 -0400

    Feat: Structure and Map

--- components/services/ServiceSmall.tsx
+++ components/services/ServiceSmall.tsx
@@ -2,11 +2,32 @@ import React from "react";
 import SmallServiceCards from "./cards/SmallServiceCards";
 
 const ServiceCards = () => {
+  const serviceData = [
+    {
+      src: "./tech/css.svg",
+      alt: "Image Alt Text 1",
+      name: "Websites",
+      description: "Project Description 1",
+    },
+    {
+      src: "./tech/css.svg",
+      alt: "Image Alt Text 2",
+      name: "Web Applications",
+      description: "Project Description 2",
+    },
+    {
+      src: "./tech/css.svg",
+      alt: "Image Alt Text 3",
+      name: "Media",
+      description: "Project Description 3",
+    },
+  ];
+
   return (
-    <div className="flex flex-col gap-[1.25rem] mx-auto justify-center items-center py-[1.25rem]">
-      <div className="w-full h-[20rem] px-[1rem]">
-        <SmallServiceCards />
-      </div>
+    <div className="flex flex-col gap-[1.25rem] mx-auto justify-center items-center py-[1.25rem] px-[1rem]">
+      {serviceData.map((data, index) => (
+        <SmallServiceCards key={index} ServiceData={data} />
+      ))}
     </div>
   );
 };
--- components/services/cards/SmallServiceCards.tsx
+++ components/services/cards/SmallServiceCards.tsx
@@ -1,11 +1,29 @@
+import Image from "next/image";
 import React from "react";
 
-const WebDev = () => {
+interface Props {
+  ServiceData: {
+    src: string;
+    alt: string;
+    name: string;
+    description: string;
+  };
+}
+
+const SmallServiceCards: React.FC<Props> = ({ ServiceData }) => {
+  const { src, alt, name, description } = ServiceData;
+
   return (
-    <div className="bg-white h-full shadow-lg rounded-lg">
-      <h3 className="flex justify-center pt-[1.5rem]">Web Development</h3>
+    <div className="bg-white h-full shadow-lg rounded-lg flex flex-col w-full p-[1rem]">
+      <div className="flex items-center justify-center">
+        <Image src={src} alt={alt} width={150} height={150} />
+      </div>
+      <div className="">
+        <h3 className="flex justify-center pt-[1.5rem]">{name}</h3>
+        <p className="flex justify-center mt-[.25rem]">{description}</p>
+      </div>
     </div>
   );
 };
 
-export default WebDev;
+export default SmallServiceCards;
