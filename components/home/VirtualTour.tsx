7c932ccd6bbdba0ac6c0233ebe68bd90651bf9d4
fcvirtual <austin@fcvt.dev>
2024-03-25 14:16:16 -0400

    fix: Homepage tour size

--- components/home/VirtualTour.tsx
+++ components/home/VirtualTour.tsx
@@ -4,7 +4,7 @@ const VirtualTour = () => {
   return (
     <div className="w-full">
       <iframe
-        className="w-full h-[33rem] max-w-[50rem] mx-auto flex border-black border-rounded border-[3px] shadow-lg rounded-lg"
+        className="w-full h-[33rem] lg:h-[42rem] max-w-[75rem] mx-auto flex border-black border-rounded border-[3px] shadow-lg rounded-lg"
         src="https://my.matterport.com/show/?m=Vu8vAVvcebN"
         allow="xr-spatial-tracking"
         allowFullScreen
