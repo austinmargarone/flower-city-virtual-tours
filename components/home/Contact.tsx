bf897a4300dfa80f2346edcd2147ab5e08d434f8
fcvirtual <austin@fcvt.dev>
2024-03-26 16:12:59 -0400

    feat: Style SVGs

--- components/home/Contact.tsx
+++ components/home/Contact.tsx
@@ -3,21 +3,21 @@ import ContactCard from "./ContactCard";
 
 const ContactData = [
   {
-    src: "./assets/icons/window-code.svg",
+    src: "./assets/icons/ContactTan.svg",
     alt: "Contact Form",
     title: "Contact Form",
     button: "/contact",
     description: "Fill out our contact form to get in touch with us",
   },
   {
-    src: "./assets/icons/window-code.svg",
+    src: "./assets/icons/PhoneTan.svg",
     alt: "Schedule Consultation",
     title: "Schedule Consultation",
     button: "/calendly",
     description: "Schedule a consultation with us to discuss your project",
   },
   {
-    src: "./assets/icons/window-code.svg",
+    src: "./assets/icons/QuoteTan.svg",
     alt: "Request a Quote",
     title: "Request a Quote",
     button: "/build",
