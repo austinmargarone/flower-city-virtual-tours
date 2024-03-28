339cdb10c9ac15569b08664676b68d1ddd16fb94
fcvirtual <austin@fcvt.dev>
2024-03-28 10:45:54 -0400

    fix: Video Frame

--- components/home/WebDeveloper.tsx
+++ components/home/WebDeveloper.tsx
@@ -6,30 +6,27 @@ const WebDeveloper = () => {
       <h2 className="h2 justify-center flex py-[1rem] text-white">
         Web Development
       </h2>
-      <div className="w-full h-[33rem] lg:h-[45rem] max-w-[77.5rem] mx-auto flex border-tan border-rounded border shadow-lg rounded-lg bg-[#040219]">
-        <div className="m-auto sm:flex hidden w-full h-full">
+      <div className="w-full h-fit max-w-[77.5rem] mx-auto flex border-tan border-rounded border shadow-lg rounded-lg bg-[#040219]">
+        <div className="m-auto xs:flex hidden w-full h-full">
           <video
             width={500}
             height={1240}
-            controls
             loop
             muted
             autoPlay
-            className="w-full h-full"
+            className="w-full h-full rounded-lg"
           >
             <source src="./WebDevIntro.mp4" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
         </div>
-        <div className="m-auto flex sm:hidden w-full h-full">
+        <div className="m-auto flex xs:hidden w-full h-full">
           <video
             width={300}
             height={900}
-            controls
-            loop
             muted
             autoPlay
-            className="w-fit h-full mx-auto flex"
+            className="w-fit h-full mx-auto flex rounded-lg"
           >
             <source src="./WebDevIntroMobile.mp4" type="video/mp4" />
             Your browser does not support the video tag.
