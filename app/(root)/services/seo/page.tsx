553014f0dd58fe2a98dc0e7af52cfe65481f148e
fcvirtual <austin@fcvt.dev>
2024-04-01 14:56:15 -0400

    feat: Web Development

--- app/(root)/services/seo/page.tsx
+++ app/(root)/services/seo/page.tsx
@@ -25,74 +25,77 @@ const Page = () => {
             converted into customers.
           </p>
         </div>
-        <div className="px-[1rem]">
+        <div className="px-[1rem] pleft">
           <h2 className="h2 text-center mt-8">Our SEO Services Cover:</h2>
           <ul className="list-disc max-w-[48rem] mx-auto text-white">
             <li>
-              <strong>Keyword Research:</strong> Identifying relevant keywords
-              and search terms related to the client's industry, products, or
-              services.
+              <strong className="pbold">Keyword Research:</strong> Identifying
+              relevant keywords and search terms related to the client's
+              industry, products, or services.
             </li>
             <li>
-              <strong>On-Page Optimization:</strong> Optimizing meta titles,
-              meta descriptions, headings, and content to improve search engine
-              visibility and relevance.
+              <strong className="pbold">On-Page Optimization:</strong>{" "}
+              Optimizing meta titles, meta descriptions, headings, and content
+              to improve search engine visibility and relevance.
             </li>
             <li>
-              <strong>Technical SEO:</strong> Addressing technical aspects of
-              website optimization, such as site speed, mobile-friendliness, URL
-              structure, and schema markup.
+              <strong className="pbold">Technical SEO:</strong> Addressing
+              technical aspects of website optimization, such as site speed,
+              mobile-friendliness, URL structure, and schema markup.
             </li>
             <li>
-              <strong>Content Optimization:</strong> Optimizing website content,
-              including blog posts, articles, and landing pages, to align with
-              target keywords and provide value to users.
+              <strong className="pbold">Content Optimization:</strong>{" "}
+              Optimizing website content, including blog posts, articles, and
+              landing pages, to align with target keywords and provide value to
+              users.
             </li>
             <li>
-              <strong>Link Building:</strong> Implementing strategies to acquire
-              high-quality backlinks from authoritative websites, directories,
-              and industry influencers to improve website authority and
-              rankings.
+              <strong className="pbold">Link Building:</strong> Implementing
+              strategies to acquire high-quality backlinks from authoritative
+              websites, directories, and industry influencers to improve website
+              authority and rankings.
             </li>
             <li>
-              <strong>Local SEO:</strong> Optimizing online presence for local
-              searches by creating and optimizing Google My Business profiles,
-              obtaining local citations, and managing reviews and ratings.
+              <strong className="pbold">Local SEO:</strong> Optimizing online
+              presence for local searches by creating and optimizing Google My
+              Business profiles, obtaining local citations, and managing reviews
+              and ratings.
             </li>
             <li>
-              <strong>SEO Audits:</strong> Conducting comprehensive audits of
-              clients' websites to identify strengths, weaknesses, and
-              opportunities for improvement, and developing action plans
-              accordingly.
+              <strong className="pbold">SEO Audits:</strong> Conducting
+              comprehensive audits of clients' websites to identify strengths,
+              weaknesses, and opportunities for improvement, and developing
+              action plans accordingly.
             </li>
             <li>
-              <strong>Competitor Analysis:</strong> Analyzing competitors'
-              websites and SEO strategies to identify areas for improvement and
-              develop strategies to gain a competitive edge.
+              <strong className="pbold">Competitor Analysis:</strong> Analyzing
+              competitors' websites and SEO strategies to identify areas for
+              improvement and develop strategies to gain a competitive edge.
             </li>
             <li>
-              <strong>Monitoring and Reporting:</strong> Monitoring website
-              performance using tools like Google Analytics and Search Console,
-              and providing regular reports on key SEO metrics, including
-              traffic, rankings, and conversions.
+              <strong className="pbold">Monitoring and Reporting:</strong>{" "}
+              Monitoring website performance using tools like Google Analytics
+              and Search Console, and providing regular reports on key SEO
+              metrics, including traffic, rankings, and conversions.
             </li>
             <li>
-              <strong>Consulting and Strategy:</strong> Providing strategic
-              guidance and consulting services to help clients define their SEO
-              objectives, select appropriate keywords, and develop customized
-              SEO strategies tailored to their business goals.
+              <strong className="pbold">Consulting and Strategy:</strong>{" "}
+              Providing strategic guidance and consulting services to help
+              clients define their SEO objectives, select appropriate keywords,
+              and develop customized SEO strategies tailored to their business
+              goals.
             </li>
             <li>
-              <strong>Algorithm Updates:</strong> Staying abreast of search
-              engine algorithm updates and industry best practices to ensure
-              that clients' websites remain optimized and compliant with current
-              SEO standards.
+              <strong className="pbold">Algorithm Updates:</strong> Staying
+              abreast of search engine algorithm updates and industry best
+              practices to ensure that clients' websites remain optimized and
+              compliant with current SEO standards.
             </li>
             <li>
-              <strong>Social Media Integration:</strong> Integrating social
-              media platforms into SEO strategies to facilitate sharing,
-              engagement, and user interaction, which can indirectly impact
-              search engine rankings.
+              <strong className="pbold">Social Media Integration:</strong>{" "}
+              Integrating social media platforms into SEO strategies to
+              facilitate sharing, engagement, and user interaction, which can
+              indirectly impact search engine rankings.
             </li>
           </ul>
         </div>
