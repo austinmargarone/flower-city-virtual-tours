2ceac90a8818269d847a1756391f75f6ca852207
austinmargarone <austin@fcvt.dev>
2024-06-12 10:43:37 -0400

    Feat: Configure Supabase for deployment

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,8 +1,8 @@
-import { createClient } from "@/utils/supabase/server";
+import { createClient } from "@supabase/supabase-js";
 
-export default async function Notes() {
-  const supabase = createClient();
-  const { data: notes } = await supabase.from("notes").select();
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
 
-  return <pre>{JSON.stringify(notes, null, 2)}</pre>;
-}
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
--- next.config.js
+++ next.config.js
@@ -1,14 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config) => {
-      config.module.rules.push({
-        test: /.svg$/i,
-        // use: ['@svgr/webpack'],
-        use: [{ loader: "@svgr/webpack", options: { icon: true } }],
-      });
-      return config;
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: [{ loader: "@svgr/webpack", options: { icon: true } }],
+    });
+    return config;
+  },
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
+};
+
+module.exports = nextConfig;
