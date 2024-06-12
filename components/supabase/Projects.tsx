62b2b230c90b91be9141a0ca8b77d31359449c98
austinmargarone <austin@fcvt.dev>
2024-06-12 16:45:43 -0400

    Fix: Shirnk projects component for very small screens

--- components/supabase/Projects.tsx
+++ components/supabase/Projects.tsx
@@ -33,7 +33,7 @@ const Test = () => {
         {projects.map((project: any) => (
           <div key={project.id}>
             <Link href={project.link} target={"_blank"} className="mx-auto">
-              <div className="bg-tan mx-auto flex w-[20rem] shrink-0 flex-col rounded-[.62725rem] bg-blue1 pb-[.32rem] pt-[2.83rem] hover:shadow-lg xs:w-[21.5rem] ss:w-[25rem] sm:h-[20.8rem] sm:w-[34.375rem] sm:pt-[4.25rem]">
+              <div className="bg-tan mx-auto flex w-[19rem] shrink-0 flex-col rounded-[.62725rem] bg-blue1 pb-[.32rem] pt-[2.83rem] hover:shadow-lg xs:w-[21.5rem] ss:w-[25rem] sm:h-[20.8rem] sm:w-[34.375rem] sm:pt-[4.25rem]">
                 <div className="mx-auto flex sm:hidden">
                   <div>
                     <Image
@@ -59,8 +59,8 @@ const Test = () => {
               </div>
               <div className="w-[20rem] mx-auto flex pl-[1.25rem] pt-[1.22rem] sm:pl-[3.6rem] sm:pt-[1.95rem] xs:w-[21.5rem] ss:w-[25rem] sm:w-[34.375rem]">
                 <div className="flex-col">
-                  <h4 className="flex text-white">{project.title}</h4>
-                  <p className="flex text-white">{project.description}</p>
+                  <h4 className="pbold flex text-white">{project.title}</h4>
+                  <p className="p flex text-white">{project.description}</p>
                 </div>
               </div>
             </Link>
