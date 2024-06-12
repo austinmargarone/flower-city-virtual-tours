98762abee2bf74ba85b000575819897bc1742667
austinmargarone <austin@fcvt.dev>
2024-06-12 10:20:24 -0400

    feat: Supabase client

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,9 +1,7 @@
-// utils/supabaseClient.js
-import { createClient } from "@supabase/supabase-js";
+import { createBrowserClient } from "@supabase/ssr";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
+export const createClient = () =>
+  createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  );
