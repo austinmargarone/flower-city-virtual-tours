b11262019d92d947238040913149f29bcad72107
austinmargarone <austin@fcvt.dev>
2024-06-13 10:39:50 -0400

    Refactor quotes

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -4,57 +4,35 @@ import React, { useEffect, useState, useRef } from "react";
 const Hero = () => {
   const [text, setText] = useState("");
   const quotes = [
-    "Engage your audience ",
-    "Boost your brand ",
-    "Drive conversions ",
-    "are tailored to your needs ",
-    "Optimize your digital presence ",
-    "Get more traffic ",
-    "Deliver experiences ",
-    "Grow your business ",
-    "Increase revenue ",
-    "Generate leads ",
-    "Convert more customers ",
-    "Improve your SEO ",
-    "empower online growth ",
-    "prioritize user experience ",
+    "Engage your audience",
+    "Boost your brand",
+    "Drive conversions",
+    "are tailored to your needs",
+    "Optimize your digital presence",
+    "Get more traffic",
+    "Deliver experiences",
+    "Grow your business",
+    "Increase revenue",
+    "Generate leads",
+    "Convert more customers",
+    "Improve your SEO",
+    "empower online growth",
+    "prioritize user experience",
   ];
   const [quoteIndex, setQuoteIndex] = useState(0);
 
   const scrollToRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    let currentIndex = 0;
-    const typingInterval = setInterval(() => {
-      setText((prevText) => {
-        if (currentIndex <= quotes[quoteIndex].length) {
-          return quotes[quoteIndex].slice(0, currentIndex++);
-        } else {
-          clearInterval(typingInterval);
-          setTimeout(() => eraseText(), 2500); // Adjust time before erasing
-          return prevText;
-        }
-      });
-    }, 100); // Adjust typing speed here
+    setText(quotes[quoteIndex]);
 
-    return () => clearInterval(typingInterval);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    const displayDuration = 2500; // Adjust display duration here
+    const interval = setInterval(() => {
+      setQuoteIndex((prevIndex) => (prevIndex + 1) % quotes.length);
+    }, displayDuration);
+
+    return () => clearInterval(interval);
   }, [quoteIndex]);
-
-  const eraseText = () => {
-    let currentIndex = quotes[quoteIndex].length;
-    const deletingInterval = setInterval(() => {
-      setText((prevText) => {
-        if (currentIndex >= 0) {
-          return quotes[quoteIndex].slice(0, currentIndex--);
-        } else {
-          clearInterval(deletingInterval);
-          setQuoteIndex((prevIndex) => (prevIndex + 1) % quotes.length);
-          return prevText;
-        }
-      });
-    }, 50); // Adjust deleting speed here
-  };
 
   const handleScroll = () => {
     if (scrollToRef.current) {
